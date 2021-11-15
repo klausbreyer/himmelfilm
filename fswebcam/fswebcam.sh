e4ba89eecfb625b7ed862d14d59750a3510cc702
kb <kb@v01.io>
2021-11-15 17:28:31 +0100

    hd call

--- fswebcam/fswebcam.sh
+++ fswebcam/fswebcam.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 DATE=$(date +"%Y-%m-%d-%H-%M-%S")
 fswebcam -r 1280x720 --no-banner /media/usb/www/www.schlachthof-wolken.de/images/$DATE.jpg
+fswebcam -r 1920x1080 --no-banner /media/usb/www/www.schlachthof-wolken.de/hd/$DATE.jpg
